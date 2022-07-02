6d053bfe939d6370a2cebe65def0f1beac1d5d07
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2022-07-02 22:34:35 +0200

    black

--- closestwins/utils.py
+++ closestwins/utils.py
@@ -8,6 +8,7 @@ from flask import abort
 
 def disable(_f):
     """Disable route decorator."""
+
     @wraps(_f)
     def decorated_function(*_args, **_kwargs):
         return abort(HTTPStatus.NOT_FOUND)
